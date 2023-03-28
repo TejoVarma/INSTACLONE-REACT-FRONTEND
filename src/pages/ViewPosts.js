548731d215a89eb06c4071446b3ee5c580c4304d
TejoVarma <tejovarma.alluri9@gmail.com>
2023-03-28 14:35:18 +0530

    All the changes are made

--- src/pages/ViewPosts.js
+++ src/pages/ViewPosts.js
@@ -4,7 +4,6 @@ import Posts from "../components/Posts";
 
 export default function ViewPosts(){
     const {posts} = useContext(List);
-    window.onload('../all');
     return <>
         {posts.length === 0? <div className="emptyDB"><h2>Oops...Create new post by clicking on the camera button...</h2></div> : 
             posts.map(post => {
